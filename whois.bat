9a2cb91f0c2397f1c37fc152ae1ea07ab31c2fa1
sathawit <37478471+sathawit@users.noreply.github.com>
2025-07-08 15:42:22 +0700

    Add files via upload

---
+++ whois.bat
@@ -0,0 +1,9 @@
+@echo off
+REM Change to the directory where your app.py is located
+cd "C:\Users\Wit\Desktop\whois"
+
+REM Specify the full path to your Python executable
+"C:\Users\Wit\Desktop\whois\Scripts\python.exe" app.py
+
+REM Optional: Keep the command prompt open after execution (useful for debugging)
+REM pause
